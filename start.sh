47f86b943c2b7b68db3c4a7c301128bafb90beaa
rjdndjsjs9 <103624181+rjdndjsjs9@users.noreply.github.com>
2024-12-31 07:53:37 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Jalankan ttyd (web terminal)
-ttyd -p 8080 bash &
+# Jalankan Xvfb untuk tampilan grafis
+Xvfb :99 -screen 0 1920x1080x24 &
 
-# Jalankan code-server (web editor)
-code-server --bind-addr 0.0.0.0:8081 --auth none --disable-telemetry --user-data-dir /root/files &
+# Jalankan Fluxbox sebagai window manager
+fluxbox &
+
+# Jalankan x11vnc untuk remote desktop
+x11vnc -display :99 -forever -nopw -rfbport 5900 &
+
+# Jalankan NoVNC untuk akses browser melalui web
+/opt/novnc/utils/launch.sh --vnc localhost:5900 --listen 8080 &
+
+# Jalankan Google Chrome di layar virtual
+google-chrome --no-sandbox --disable-dev-shm-usage --start-maximized --display=:99 &
 
 # Menjaga container tetap berjalan
 tail -f /dev/null
